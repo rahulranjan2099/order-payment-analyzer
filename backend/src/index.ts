ba3bdab2c5a681258310266c94492a95d111f626
rahulranjan2099 <rahulranjantom@gmail.com>
2026-07-19 14:15:26 +0530

    added cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,7 +15,13 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    credentials: true,
+  })
+)
 app.use(express.json());
 app.get("/health", (_req, res) => {
   res.json({ status: "ok" });
